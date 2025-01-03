c4b074838a35934c2c35a96a5689ba373b749c86
dee230803 <2023663362@student.uitm.edu.my>
2025-01-04 06:32:02 +0800

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-af289","appId":"1:981594704127:web:6f8d1fd459f78e567224ca","storageBucket":"friendlychat-af289.firebasestorage.app","apiKey":"AIzaSyAAgFZv8AeUK-4cyWksDkZpaMz_73p5fHA","authDomain":"friendlychat-af289.firebaseapp.com","messagingSenderId":"981594704127"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
