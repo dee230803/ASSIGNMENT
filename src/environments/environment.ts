5801dc689686bbd39d4d9133978680e90658b654
dee230803 <2023663362@student.uitm.edu.my>
2025-01-04 06:41:16 +0800

    import and configure firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,17 @@
-export const environment = {
-  firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAgFZv8AeUK-4cyWksDkZpaMz_73p5fHA",
+  authDomain: "friendlychat-af289.firebaseapp.com",
+  projectId: "friendlychat-af289",
+  storageBucket: "friendlychat-af289.firebasestorage.app",
+  messagingSenderId: "981594704127",
+  appId: "1:981594704127:web:6f8d1fd459f78e567224ca"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
