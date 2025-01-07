0c456606faa332af60f9a9e78807f9e76c536a11
dee230803 <2023663362@student.uitm.edu.my>
2025-01-08 01:22:48 +0800

    Add the ability to post images

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAgFZv8AeUK-4cyWksDkZpaMz_73p5fHA",
+    authDomain: "friendlychat-af289.firebaseapp.com",
+    projectId: "friendlychat-af289",
+    storageBucket: "friendlychat-af289.firebasestorage.app",
+    messagingSenderId: "981594704127",
+    appId: "1:981594704127:web:6f8d1fd459f78e567224ca"
+  }; 
